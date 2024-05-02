fc4b7856874ee39a9b6f0aa33807b21aa6efd32c
wnghks7787 <wnghks7787@naver.com>
2024-05-02 16:32:13 +0900

    Strategy_code1: No design pattern

---
+++ src/TaekwonV.java
@@ -0,0 +1,14 @@
+public class TaekwonV extends Robot {
+
+    public TaekwonV(String name) {
+        super(name);
+    }
+
+    public void attack() {
+        System.out.println("I have missile and can attack with it.");
+    }
+
+    public void move() {
+        System.out.println("I can only walk.");
+    }
+}
